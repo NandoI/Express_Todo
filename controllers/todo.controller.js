4a342d1649c39a5c00822ff0977c4c1a31156eb6
Faishal Fernando Hutama <125978885+NandoI@users.noreply.github.com>
2023-11-13 21:06:55 +0700

    membenarkan kesalahan hubangan table user dan todos

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,9 +1,11 @@
-const { Todo } = require('../models'); // Pastikan ini sesuai dengan struktur folder dan model Anda
+const { Todo,User } = require('../models'); // Pastikan ini sesuai dengan struktur folder dan model Anda
 
 module.exports = {
     getAllTodo: async (req, res) => {
         try {
-            const todos = await Todo.findAll();
+            const todos = await Todo.findAll({
+                include: User
+            });
             res.status(200).json({
                 message: "Berhasil mendapatkan todo",
                 data: todos
@@ -11,6 +13,25 @@ module.exports = {
         } catch (error) {
             res.status(500).json({
                 message: "Gagal mendapatkan data",
+                error: error.message
+            });
+        }
+    },
+
+    addTodo: async (req,res) => {
+        const { title, description, userId } = req.body; // Di sini, userId akan menunjukkan pengguna yang terkait dengan Todo
+
+        try {
+            // Membuat entitas Todo baru dan menyimpannya ke dalam basis data
+            const newTodo = await Todo.create({ title, description, userId });
+    
+            res.status(201).json({
+                message: "Todo berhasil ditambahkan",
+                todo: newTodo // Mengembalikan data Todo yang baru saja dibuat
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal menambahkan Todo",
                 error: error.message
             });
         }
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllTodo } = require('../controllers/todo.controller'); // Sesuaikan path dengan struktur folder Anda
+const { getAllTodo, addTodo } = require('../controllers/todo.controller'); // Sesuaikan path dengan struktur folder Anda
 
 const route = express.Router();
 
 route.get('/', getAllTodo);
+route.post('/', addTodo)
 
 module.exports = route;

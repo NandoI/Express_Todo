baf2560fdfc3d4188a2fc1c7cfc230620c1a7a39
Faishal Fernando Hutama <125978885+NandoI@users.noreply.github.com>
2023-11-13 21:25:32 +0700

    menambahkan delete todo, getby id todos

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,4 +1,4 @@
-const { Todo,User } = require('../models'); // Pastikan ini sesuai dengan struktur folder dan model Anda
+const { Todo,User } = require('../models');
 
 module.exports = {
     getAllTodo: async (req, res) => {
@@ -19,15 +19,14 @@ module.exports = {
     },
 
     addTodo: async (req,res) => {
-        const { title, description, userId } = req.body; // Di sini, userId akan menunjukkan pengguna yang terkait dengan Todo
+        const { title, description, userId } = req.body;
 
         try {
-            // Membuat entitas Todo baru dan menyimpannya ke dalam basis data
             const newTodo = await Todo.create({ title, description, userId });
     
             res.status(201).json({
                 message: "Todo berhasil ditambahkan",
-                todo: newTodo // Mengembalikan data Todo yang baru saja dibuat
+                todo: newTodo
             });
         } catch (error) {
             res.status(500).json({
@@ -35,5 +34,57 @@ module.exports = {
                 error: error.message
             });
         }
-    }
+    },
+
+    deleteTodo: async (req, res) => {
+        try {
+        
+        const todosId = req.params.id;
+        const todos = await Todo.findByPk(todosId)
+
+        if(!todos) {
+            return res.status(404).json(
+                {
+                    message: "todo tidak ditemukan"
+                }
+            )
+        }
+
+        await todos.destroy()
+        res.json({
+            message:"todo berhasil dihapus"
+        })
+        } catch (error) {
+            res.status(500).json(
+                {
+                    message: 'gagal menghapus todos',
+                    error:error.message
+                }
+            )
+            
+        }
+    },
+
+    getTodoById: async (req, res) => {
+        try {
+            const todoId = req.params.id;
+            const todo = await Todo.findByPk(todoId, { include: User });
+
+            if (!todo) {
+                return res.status(404).json({
+                    message: "Todo tidak ditemukan"
+                });
+            }
+
+            res.json({
+                message: "Berhasil mendapatkan data todo",
+                data: todo
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: 'Gagal mendapatkan data todo',
+                error: error.message
+            });
+        }
+    },
 };
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllTodo, addTodo } = require('../controllers/todo.controller'); // Sesuaikan path dengan struktur folder Anda
+const { getAllTodo, addTodo, deleteTodo, getTodoById } = require('../controllers/todo.controller'); // Sesuaikan path dengan struktur folder Anda
 
 const route = express.Router();
 
 route.get('/', getAllTodo);
-route.post('/', addTodo)
+route.post('/', addTodo);
+route.delete('/:id', deleteTodo)
+route.get('/:id', getTodoById)
 
 module.exports = route;
